e167d4a00e6993b6cb8331d3191cb880f818c8f9
Lucas Emmanuel Cabrera <lucacabrera@ar0c02dt4wvml85.fibertel.com.ar>
2021-04-07 17:44:15 -0300

    Add exceptions

--- src/main/java/com/bootcamp/finalProject/controllers/PartController.java
+++ src/main/java/com/bootcamp/finalProject/controllers/PartController.java
@@ -1,7 +1,11 @@
 package com.bootcamp.finalProject.controllers;
 
+import com.bootcamp.finalProject.dtos.ErrorDTO;
+import com.bootcamp.finalProject.exceptions.InternalExceptionHandler;
 import com.bootcamp.finalProject.services.PartService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -13,4 +17,11 @@ public class PartController {
     PartService service;
 
     //TODO: ARMAR el o los ENDPOINT.
+
+
+    @ExceptionHandler(InternalExceptionHandler.class)
+    public ResponseEntity<ErrorDTO> handleException(InternalExceptionHandler e)
+    {
+        return new ResponseEntity<>(e.getError(), e.getReturnStatus());
+    }
 }
