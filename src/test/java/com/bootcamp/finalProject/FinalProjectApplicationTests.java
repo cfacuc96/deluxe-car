0a5f8f94569c5d046c99ad1689ac40c2a7c7e5bb
jcagideML <joaquin.cagide@mercadolibre.com>
2021-04-07 12:44:48 -0300

    Init commit

---
+++ src/test/java/com/bootcamp/finalProject/FinalProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bootcamp.finalProject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FinalProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
