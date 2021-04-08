b9e67141ab509fad18074365435ed750f2f97734
jcagideML <joaquin.cagide@mercadolibre.com>
2021-04-08 13:21:07 -0300

    Nulleable fields added

--- src/main/java/com/bootcamp/finalProject/model/Part.java
+++ src/main/java/com/bootcamp/finalProject/model/Part.java
@@ -22,19 +22,21 @@ public class Part {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name = "id_part")
     private Long idPart;
-    @Column(name = "part_code", length = 8)
+    @Column(name = "part_code", nullable = false, length = 8)
     private Integer partCode;
     @Column(length = 100)
     private String description;
-    @Column(length = 4)
+    @Column(length = 4, nullable = false)
     private Integer widthDimension;
-    @Column(length = 4)
+    @Column(length = 4, nullable = false)
     private Integer tallDimension;
-    @Column(length = 4)
+    @Column(length = 4, nullable = false)
     private Integer longDimension;
-    @Column(length = 5)
+    @Column(length = 5, nullable = false)
     private Integer netWeight;
+    @Column(nullable = false)
     private Integer quantity;
+    @Column(nullable = false)
     private Date lastModification; //Se encarga de guardar la fecha de la ultima modificacion tanto de precio como un atributo
 
     @OrderBy("created_at DESC") //Trae la lista de partRecords ordenada por fecha de createAt (Primero trae la ultima modificacion agregada)
