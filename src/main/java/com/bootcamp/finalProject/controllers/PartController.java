77138d70f915330b62b5adc51db4a9db31e7b2c1
JulianCBenavides <juliancamilo.benavides@mercadolibre.com.co>
2021-04-08 12:16:49 -0500

    *

--- src/main/java/com/bootcamp/finalProject/controllers/PartController.java
+++ src/main/java/com/bootcamp/finalProject/controllers/PartController.java
@@ -35,8 +35,8 @@ public class PartController {
     @GetMapping("list")
     public List<PartResponseDTO> obtainList(@Nullable @RequestParam Map<String, String> params) throws Exception {
         PartRequestDTO requestDTO = new PartRequestDTO();
-        requestDTO.setQueryType(params.get("querytype"));
-        requestDTO.setDate(validateDateFormat(params.get("date")));
+        requestDTO.setQueryType(params.get("queryType"));
+        requestDTO.setDate((params.get("date") == null) ? null : validateDateFormat(params.get("date")));
         requestDTO.setOrder((params.get("order")==null)? 0: Integer.parseInt(params.get("order")));
         return service.findPart(requestDTO);
     }
