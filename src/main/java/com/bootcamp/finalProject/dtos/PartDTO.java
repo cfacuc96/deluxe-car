7bb7ac41f3a2cec5c5279addb387a270145b52b2
tepanioDevMeli <martin.darosa@mercadolibre.com>
2021-04-12 14:15:41 -0300

    ..

--- src/main/java/com/bootcamp/finalProject/dtos/PartDTO.java
+++ src/main/java/com/bootcamp/finalProject/dtos/PartDTO.java
@@ -1,2 +1,20 @@
-package com.bootcamp.finalProject.dtos;public class PartDTO {
+package com.bootcamp.finalProject.dtos;
+
+public class PartDTO {
+
+    private Integer partCode;
+    private String description;
+    private Integer quantity;
+    private Integer netWeight;
+    private Integer longDimension;
+    private Integer widthDimension;
+    private Integer tallDimension;
+
+    private Double normalPrice;
+    private Double salePrice;
+    private Double urgentPrice;
+
+
+    private Long makerId;
+    private Long discountId;
 }
