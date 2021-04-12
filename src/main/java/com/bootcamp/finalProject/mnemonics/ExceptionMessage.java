8e76fc1e07fbebc05c8aebbe701389f59df2831e
JulianCBenavides <juliancamilo.benavides@mercadolibre.com.co>
2021-04-12 09:33:37 -0500

    editing in the handling of exception messages through the use of a mnemonic

--- src/main/java/com/bootcamp/finalProject/mnemonics/ExceptionMessage.java
+++ src/main/java/com/bootcamp/finalProject/mnemonics/ExceptionMessage.java
@@ -5,4 +5,15 @@ public class ExceptionMessage {
     public static final String NOT_A_NUMBER = "The order might be a number.";
 
     public static final String EMPTY_PARAMS = "Empty parameters.";
+
+    public static final String DATE_NOT_NECESSARY = "for a complete search the date is not necessary";
+
+    public static final String DATE_IS_NECESSARY = "for a date it is necessary to " +
+            "specify if the query will be of partial type or of variation type";
+
+    public static final String MISSING_DEALER_NUMBER = "dealerNumber is missing from request";
+
+    public static final String NOT_CONTAINS_DELIVERY_STATUS = "deliveryStatus not in ";
+
+
 }
