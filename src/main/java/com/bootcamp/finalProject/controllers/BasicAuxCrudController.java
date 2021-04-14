12001f93246ae363262ce8f2ea353e7435689158
JulianCBenavides <juliancamilo.benavides@mercadolibre.com.co>
2021-04-14 16:04:20 -0500

    add Central controller with the handlers exception

--- src/main/java/com/bootcamp/finalProject/controllers/BasicAuxCrudController.java
+++ src/main/java/com/bootcamp/finalProject/controllers/BasicAuxCrudController.java
@@ -19,7 +19,7 @@ import java.util.List;
 
 @RestController
 @RequestMapping("/api/v1/parts")
-public class BasicAuxCrudController {
+public class BasicAuxCrudController extends CentralController{
 
     @Autowired
     IPartService service;
